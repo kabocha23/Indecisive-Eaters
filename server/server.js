9aa55e625ad5e21e3cad165a5641ee5e294ee5b4
kabocha23 <jekobuchi@gmail.com>
2018-08-29 22:38:39 -0700

    Added root path in express server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config({path: '../.env'});
 
 const express = require('express');
-const session = require('express-session');
+const path = require('path');
+// const session = require('express-session');
 const cors = require('cors');
 const app = express();
 const bodyParser = require("body-parser");
@@ -11,20 +12,24 @@ const yelpController = require('./controllers/yelpController');
 
 app.use(bodyParser.json());
 
-app.use(session({
-  secret: process.env.EXPRESS_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 14
-  }
-}))
+// app.use(session({
+//   secret: process.env.EXPRESS_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     maxAge: 1000 * 60 * 60 * 24 * 14
+//   }
+// }))
 
 app.use(cors());
 
 app.use(express.static('../build/'));
 
 app.get('/api/restaurantsearch', yelpController.regularSearch)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+});
 
 app.listen(PORT, () => 
   console.log(`Listening on port: ${PORT}`)
